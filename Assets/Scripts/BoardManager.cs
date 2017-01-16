2b9b81bdecbbd1ec915182d62b972e1ba2abebf6
grimskin <grimskin@gmail.com>
2017-01-16 22:11:07 +0200

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;       //Allows us to use Lists.
+
+public class BoardManager : MonoBehaviour
+{
+	// Using Serializable allows us to embed a class with sub properties in the inspector.
+	[Serializable]
+	public class Count
+	{
+		public int minimum;             //Minimum value for our Count class.
+		public int maximum;             //Maximum value for our Count class.
+
+
+		//Assignment constructor.
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+
+	public int columns = 10;                                         //Number of columns in our game board.
+	public int rows = 10;                                            //Number of rows in our game board.
+	public GameObject[] floorTiles;                                 //Array of floor prefabs.
+
+	private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
+	private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
+
+	//Clears our list gridPositions and prepares it to generate a new board.
+	void InitialiseList ()
+	{
+		//Clear our list gridPositions.
+		gridPositions.Clear ();
+
+		//Loop through x axis (columns).
+		for(int x = 0; x < columns; x++)
+		{
+			//Within each column, loop through y axis (rows).
+			for(int y = 0; y < rows; y++)
+			{
+				//At each index add a new Vector3 to our list with the x and y coordinates of that position.
+				gridPositions.Add (new Vector3(x, y, 0f));
+			}
+		}
+	}
+
+
+	//Sets up the outer walls and floor (background) of the game board.
+	void BoardSetup ()
+	{
+		//Instantiate Board and set boardHolder to its transform.
+		boardHolder = new GameObject ("Board").transform;
+
+		//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+		for(int x = 0; x < columns; x++)
+		{
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for(int y = 0; y < rows; y++)
+			{
+				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+//				GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
+				GameObject toInstantiate = Resources.Load<GameObject>("Prefabs/TerrainTile");
+				Debug.Log (toInstantiate);
+
+				//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+				GameObject instance =
+					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+
+				//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
+		
+	//SetupScene initializes our level and calls the previous functions to lay out the game board
+	public void SetupScene ()
+	{
+		//Creates the outer walls and floor.
+		BoardSetup ();
+
+		//Reset our list of gridpositions.
+		InitialiseList ();
+	}
+}
