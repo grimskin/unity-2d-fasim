e0493ee676a95f25132f419d723cc99994004490
grimskin <grimskin@gmail.com>
2017-01-16 23:18:45 +0200

    Removed unused code

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -26,26 +26,6 @@ public class BoardManager : MonoBehaviour
 	public GameObject[] floorTiles;                                 //Array of floor prefabs.
 
 	private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
-	private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
-
-	//Clears our list gridPositions and prepares it to generate a new board.
-	void InitialiseList ()
-	{
-		//Clear our list gridPositions.
-		gridPositions.Clear ();
-
-		//Loop through x axis (columns).
-		for(int x = 0; x < columns; x++)
-		{
-			//Within each column, loop through y axis (rows).
-			for(int y = 0; y < rows; y++)
-			{
-				//At each index add a new Vector3 to our list with the x and y coordinates of that position.
-				gridPositions.Add (new Vector3(x, y, 0f));
-			}
-		}
-	}
-
 
 	//Sets up the outer walls and floor (background) of the game board.
 	void BoardSetup ()
@@ -53,26 +33,20 @@ public class BoardManager : MonoBehaviour
 		//Instantiate Board and set boardHolder to its transform.
 		boardHolder = new GameObject ("Board").transform;
 
-		//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
-		for(int x = 0; x < columns; x++)
-		{
+		for (int x = 0; x < columns; x++) {
 			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
-			for(int y = 0; y < rows; y++)
-			{
-				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
-//				GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
-				GameObject toInstantiate = Resources.Load<GameObject>("Prefabs/TerrainTile");
+			for (int y = 0; y < rows; y++) {
+				GameObject toInstantiate = Resources.Load<GameObject> ("Prefabs/TerrainTile");
 
-				//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
-				GameObject instance =
+				GameObject goInstance =
 					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
 
 				Sprite[] sprites = Resources.LoadAll<Sprite> ("Sprites/Terrain/terrain_1");
 
-				instance.GetComponent<SpriteRenderer> ().sprite = sprites [47];
+				goInstance.GetComponent<SpriteRenderer> ().sprite = sprites [47];
 
 				//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-				instance.transform.SetParent (boardHolder);
+				goInstance.transform.SetParent (boardHolder);
 			}
 		}
 	}
@@ -82,8 +56,5 @@ public class BoardManager : MonoBehaviour
 	{
 		//Creates the outer walls and floor.
 		BoardSetup ();
-
-		//Reset our list of gridpositions.
-		InitialiseList ();
 	}
 }
