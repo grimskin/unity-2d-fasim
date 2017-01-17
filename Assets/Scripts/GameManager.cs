3f1f1828a4a737a66dab02383caacbe72474195e
grimskin <grimskin@gmail.com>
2017-01-17 09:46:46 +0200

    Now we can move camera with arrow keys

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 using System.Collections.Generic;       //Allows us to use Lists. 
 
+
 public class GameManager : MonoBehaviour
 {
 	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
-	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+
+	public BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
 
 	//Awake is always called before any Start functions
 	void Awake()
