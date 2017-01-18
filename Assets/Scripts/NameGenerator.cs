d946da158c69c1fb4fbc0c7e088c50a92aa72dc1
grimskin <grimskin@gmail.com>
2017-01-18 09:08:12 +0200

    Introduced name generation to UnitController

---
+++ Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NameGenerator {
+
+	private static List<string> firstNamesDict;
+
+	private static List<string> lastNamesDict;
+
+	public static string firstName() {
+		if (firstNamesDict == null) {
+			firstNamesDict = new List<string>();
+
+			firstNamesDict.Add ("Torin");
+			firstNamesDict.Add ("Fili");
+			firstNamesDict.Add ("Kili");
+			firstNamesDict.Add ("Balin");
+			firstNamesDict.Add ("Dwalin");
+			firstNamesDict.Add ("Oin");
+			firstNamesDict.Add ("Gloin");
+			firstNamesDict.Add ("Dori");
+			firstNamesDict.Add ("Nori");
+			firstNamesDict.Add ("Ori");
+			firstNamesDict.Add ("Bifur");
+			firstNamesDict.Add ("Bofur");
+			firstNamesDict.Add ("Bombur");
+		}
+
+		return firstNamesDict[Random.Range (0,firstNamesDict.Count)];
+	}
+
+	public static string lastName() {
+		if (lastNamesDict == null) {
+			lastNamesDict = new List<string>();
+
+			lastNamesDict.Add ("Oakenshield");
+			lastNamesDict.Add ("Gundabad");
+			lastNamesDict.Add ("Belegost");
+			lastNamesDict.Add ("Nogrod");
+			lastNamesDict.Add ("Hithaeglir");
+		}
+
+		return lastNamesDict[Random.Range (0,lastNamesDict.Count)];
+	}
+
+	public static string fullName() {
+		return NameGenerator.firstName () + ' ' + NameGenerator.lastName ();
+	}
+}
--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class UnitController {
 
+	public const string STATE_IDLE = "STATE_IDLE";
+	public const string STATE_MOVING = "STATE_MOVING";
+
 	public int column;
 
 	public int row;
 
+	public string state;
+
 	public GameObject avatar;
+
+	public string name;
+
+	private Vector3 movingTo;
+
+	private float movingSpeed;
 
 
 	public void Start () {
 		// position our guy at the initial location
 		avatar.transform.position = new Vector3 (column, row, 0f);
+		state = STATE_IDLE;
+		if (name == null) {
+			name = NameGenerator.fullName();
+		}
+
+		Debug.Log (name + " came to this world");
 	}
-	
 
 	public void Update () {
-		
+		if (state == STATE_MOVING) {
+			continueMoving ();
+			return;
+		}
+	}
+
+	void continueMoving() {
 	}
 }
